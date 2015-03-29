f21c2941fcc2edb4295811296d56d3cb5b2e57a8
Nicolas Jouanin <nico@beerfactory.org>
2015-03-29 22:02:32 +0200

    Add sample status and choices

--- data_brewer/models.py
+++ data_brewer/models.py
@@ -10,15 +10,22 @@ class DataSource(models.Model):
 
 
 class DataStream(models.Model):
-    name = models.CharField(max_length=40)
-    description = models.CharField(max_length=1000, null=True, blank=True)
-    metadata = jsonfield.JSONField()
+    name = models.CharField(_('Stream name'),  max_length=40)
+    description = models.CharField(_('Stream description'), max_length=1000, null=True, blank=True)
+    metadata = jsonfield.JSONField(_('Stream meta-data'))
     data_source = models.ForeignKey(DataSource)
 
 
+SAMPLE_STATUS = (
+    ('NEW', _('New sample')),
+    ('OBSOLETE', _('Obsolete sample')),
+)
+
+
 class DataSample(models.Model):
-    metadata = jsonfield.JSONField()
-    sample_date = models.DateTimeField(null=False)
-    record_date = models.DateTimeField(null=False, auto_now_add=True, editable=False)
-    value = models.CharField(max_length=100)
+    metadata = jsonfield.JSONField(_('Sample metadata'))
+    sample_date = models.DateTimeField(_('Sample sampling date'), null=False)
+    record_date = models.DateTimeField(_('Sample record date'), null=False, auto_now_add=True, editable=False)
+    value = models.CharField(_('Sample value'), max_length=100)
     data_stream = models.ForeignKey(DataStream)
+    status = models.CharField(max_length=10, choices=SAMPLE_STATUS)
