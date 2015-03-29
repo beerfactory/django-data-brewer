d0bbe800c004feed39dd2ebdafb8bbb9717fd887
Nicolas Jouanin <nico@beerfactory.org>
2015-03-29 21:46:36 +0200

    Add atributes

--- data_brewer/models.py
+++ data_brewer/models.py
@@ -1,21 +1,24 @@
+from django.utils.translation import ugettext as _
 from django.db import models
 import jsonfield
 
 
 class DataSource(models.Model):
-    name = models.CharField(max_length=40)
-    description = models.CharField(max_length=1000, null=True)
-    metadata = jsonfield.JSONField()
+    name = models.CharField(_('Data source name'),  max_length=40)
+    description = models.CharField(_('Data source description'), max_length=1000, null=True, blank=True)
+    metadata = jsonfield.JSONField(_('Data source meta-data'))
+
 
 class DataStream(models.Model):
     name = models.CharField(max_length=40)
-    description = models.CharField(max_length=1000, null=True)
+    description = models.CharField(max_length=1000, null=True, blank=True)
     metadata = jsonfield.JSONField()
     data_source = models.ForeignKey(DataSource)
 
 
-class DataPoint(models.Model):
+class DataSample(models.Model):
     metadata = jsonfield.JSONField()
-    timestamp = models.DateTimeField(null=False)
+    sample_date = models.DateTimeField(null=False)
+    record_date = models.DateTimeField(null=False, auto_now_add=True, editable=False)
     value = models.CharField(max_length=100)
     data_stream = models.ForeignKey(DataStream)
