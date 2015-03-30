d2838f32a5bd23e97cf77685fb41f5e2dd8f2b27
Nicolas Jouanin <nico@beerfactory.org>
2015-03-30 22:01:41 +0200

    Add model classes to admin site

--- data_brewer/admin.py
+++ data_brewer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from data_brewer.models import DataSource, DataStream, Sample
 
 # Register your models here.
+admin.site.register(DataSource)
+admin.site.register(DataStream)
+admin.site.register(Sample)
